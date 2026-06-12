0cd3d79eeb836e81b6ddb7c53be692ad5baa8340
MultiRight <mohssine.dibt@protonmail.com>
2026-06-12 19:30:22 +0100

    Refactor RAM monitor with improved status handling

--- RAM-monitor.py
+++ RAM-monitor.py
@@ -7,14 +7,21 @@
 import psutil
 import sys
 import time 
+import signal
+
+# Fix Ctrl + c exit
+
+signal.signal(signal.SIGINT,signal.default_int_handler)
 
 # Enable ANSI escape codes on Windows (not needed on Linux/Mac)
+
 if sys.platform == "win32":
     import os
     os.system("")
-    clear = lambda: os.system("cls")
-else:
-    clear = lambda: print("\033c", end="")
+    
+# clear this screen to strat a application 
+
+print("\033c", end="")
 
 # color variables
 
@@ -23,6 +30,8 @@ color_green = "\033[32m"
 color_orange = "\033[33m"
 color_light_blue = "\033[94m"
 color_reset = "\033[0m"
+
+# while status
 
 running = True
 
@@ -43,91 +52,53 @@ while running :
     total_rounded = (round(total_ram / (1024 ** 3) , 2))
     used_rounded = (round(used_ram / (1024 ** 3) , 2))
     available_rounded = (round(available_ram / (1024 ** 3) , 2))
+    
+    # status ram usage levels
+
+    if percent_ram >= 80 :
+        color_per = color_red
+        status = "Bad       "
+        ram_desc = "Extremely High Ram usage "
+
+    elif 60 <= percent_ram < 80 :
+        color_per = color_orange
+        status = "Good      "
+        ram_desc = "Medium Ram Usage          "
+        
+
+    else : 
+        color_per = color_green
+        status = "Very Good "
+        ram_desc = "low ram usage              "
 
     # Display consumption data
 
-    if percent_ram >= 80 :
-        try :
-            print(f"{color_light_blue}======================================RAM monitor======================================{color_reset}")
+    try :
+        print(f"{color_light_blue}======================================RAM-monitor======================================{color_reset}")
 
-            print()
+        print()
 
-            print(f"{color_red}total Ram :{total_rounded}{color_reset}")
-            print(f"{color_red}usage Ram : {used_rounded}{color_reset}")
-            print(f"{color_red}available Ram : {available_rounded}{color_reset}")
-            print(f"{color_red}usage Ram % : {percent_ram}{color_reset}")
+        print(f"{color_per}total Ram :{total_rounded} GB    {color_reset}")
+        print(f"{color_per}usage Ram : {used_rounded} GB    {color_reset}")
+        print(f"{color_per}available Ram : {available_rounded} GB    {color_reset}")
+        print(f"{color_per}usage Ram % : {percent_ram} %  {color_reset}")
 
-            print()
+        print()
 
-            print(f"{color_light_blue}Extremely High Ram usage{color_reset}")
-            print(f"Status : {color_red}Bad{color_reset}")
+        print(f"{color_light_blue}{ram_desc}{color_reset}")
+        print(f"Status : {color_per}{status}{color_reset}")
 
-            time.sleep(0.5)
-            clear()
+        time.sleep(0.5)
+        print("\033[H" , end="")
 
-        except KeyboardInterrupt:
+    except KeyboardInterrupt:
+        running = False
+        print()
 
-            running = False
-            print()
+        print(f"{color_orange}Thank you for using RAM-monitor!{color_reset}")
+        print(f"{color_orange}Author : https://github.com/MultiRight{color_reset}")
 
-            print(f"{color_orange}Thank you for using RAM monitor!{color_reset}")
-            print(f"{color_orange}Author : https://github.com/MultiRight{color_reset}")
+        break
 
-            
-            break
 
-    elif 60 <= percent_ram < 80 :
-        try :
-            print(f"{color_light_blue}======================================RAM monitor======================================{color_reset}")
-
-            print()
-
-            print(f"{color_orange}total Ram :{total_rounded}{color_reset}")
-            print(f"{color_orange}usage Ram : {used_rounded}{color_reset}")
-            print(f"{color_orange}available Ram : {available_rounded}{color_reset}")
-            print(f"{color_orange}usage Ram % : {percent_ram}{color_reset}")
-            print()
-
-            print(f"{color_light_blue}Medium Ram Usage{color_reset}")
-            print(f"Status : {color_orange}Good{color_reset}")
-
-            time.sleep(0.5)
-            clear()
-
-        except KeyboardInterrupt:
-            running = False
-            print()
-
-            print(f"{color_orange}Thank you for using RAM monitor!{color_reset}")
-            print(f"{color_orange}Author : https://github.com/MultiRight{color_reset}")
-
-            break
-
-    else :
-        try :
-            print(f"{color_light_blue}======================================RAM monitor======================================{color_reset}")
-
-            print()
-
-            print(f"{color_green}total Ram :{total_rounded}{color_reset}")
-            print(f"{color_green}usage Ram : {used_rounded}{color_reset}")
-            print(f"{color_green}available Ram : {available_rounded}{color_reset}")
-            print(f"{color_green}usage Ram % : {percent_ram}{color_reset}")
-
-            print()
-
-            print(f"{color_light_blue}low ram usage{color_reset}")
-            print(f"Status : {color_green}Very Good{color_reset}")
-
-            time.sleep(0.5)
-            clear()
-
-        except KeyboardInterrupt:
-            running = False
-            print()
-
-            print(f"{color_orange}Thank you for using RAM monitor!{color_reset}")
-            print(f"{color_orange}Author : https://github.com/MultiRight{color_reset}")
-
-            break
 
